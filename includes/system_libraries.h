ef305522eb192df9cf32667540d336df882a06cb
4slan <amalsago@student.42.fr>
2020-03-12 10:16:48 +0100

    add system_libraries.h

---
+++ includes/system_libraries.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   system_libraries.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/12 09:47:56 by amalsago          #+#    #+#             */
+/*   Updated: 2020/03/12 09:56:46 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SYSTEM_LIBRARIES_H
+# define SYSTEM_LIBRARIES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+/*
+** SYSTEM_LIBRARIES_H
+*/
+#endif
