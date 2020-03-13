dce054722e5762901e03b90208a83d58795ea45c
4slan <amalsago@student.42.fr>
2020-03-13 22:21:51 +0100

    definition of SUCCESS, FAILURE and print_help()

--- includes/lemin.h
+++ includes/lemin.h
@@ -6,14 +6,23 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 09:54:34 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/12 10:17:22 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/13 20:37:25 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 
+# include "libft.h"
+# include "colors.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
 # include "system_libraries.h"
+
+# define SUCCESS 0
+# define FAILURE 1
+
+void		print_help(void);
 
 /*
 ** LEMIN_H
