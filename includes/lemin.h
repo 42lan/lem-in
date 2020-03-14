d6a9f213e004c0970419fbce3709999f9d524cba
4slan <amalsago@student.42.fr>
2020-03-14 22:21:52 +0100

    add EMF, define raw structs

--- includes/lemin.h
+++ includes/lemin.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 09:54:34 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/13 20:37:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/14 22:21:00 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,32 @@
 # include "get_next_line.h"
 # include "system_libraries.h"
 
+// Error Management Flag - activable on make EMF="-D EMF=1"
+# ifndef EMF
+#  define EMF 0
+# endif
+
 # define SUCCESS 0
 # define FAILURE 1
 
-void		print_help(void);
+typedef struct		s_link
+{
+
+}					t_link;
+
+typedef struct		s_room
+{
+	char			*name;
+	int				coord_x;
+	int				coord_y;
+}					t_room;
+
+typedef struct		s_farm
+{
+	int				number_of_ants;
+	t_room			*rooms;
+	t_link			*links;
+}					t_farm;
 
 /*
 ** LEMIN_H
