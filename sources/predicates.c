5593729690f043c2d10f2f63dbd0c421d80d116a
4slan <amalsago@student.42.fr>
2020-03-14 22:24:08 +0100

    feat: add predicators for parsing

---
+++ sources/predicates.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   predicates.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/14 22:14:44 by amalsago          #+#    #+#             */
+/*   Updated: 2020/03/14 22:16:24 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lemin.h"
+
+int			is_number(const char *str)
+{
+	int		i;
+
+	i = -1;
+	while (str[++i])
+		if (!ft_isdigit(str[i]))
+			return (FAILURE);
+	return (SUCCESS);
+}
+
+int			is_command(const char *str)
+{
+	if (ft_strequ(str, "##start") || ft_strequ(str, "##end"))
+		return (SUCCESS);
+	return (FAILURE);
+}
+
+int			is_comment(const char *str)
+{
+	if (str[0] == '#' && is_command(str) == FAILURE)
+		return (SUCCESS);
+	return (FAILURE);
+}
