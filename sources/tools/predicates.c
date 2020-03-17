7aa0054041472efb33bfd6b6108c359918864cb8
4slan <amalsago@student.42.fr>
2020-03-17 17:40:42 +0100

    feat: add predicates for room and link

--- sources/tools/predicates.c
+++ sources/tools/predicates.c
@@ -6,11 +6,31 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/14 22:14:44 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/17 10:34:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/17 14:37:14 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
+
+int					is_link(char *line)
+{
+	int				words;
+
+	words = ft_count_words(line, '-');
+	if (words == 2)
+		return (SUCCESS);
+	return (FAILURE);
+}
+
+int					is_room(char *line)
+{
+	int				words;
+
+	words = ft_count_words(line, ' ');
+	if (words == 3)
+		return (SUCCESS);
+	return (FAILURE);
+}
 
 int			is_number(const char *str)
 {
