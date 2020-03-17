89d399a9345cbfb31fbe34c427c521b81e3015bd
aboscho <argan.bosch@free.fr>
2020-03-17 19:15:35 +0100

    fix: crash because off NULL->

--- sources/initialization.c
+++ sources/initialization.c
@@ -6,26 +6,19 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/16 22:16:49 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/16 22:33:51 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/17 19:13:47 by abosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
+#include <stdio.h>
 static void		initialize_data(t_data *data)
 {
 	data->raw_input = NULL;
 }
 
-static void		initialize_farm(t_farm *farm)
-{
-	farm->number_of_ants = -1;
-	farm->rooms = NULL;
-	farm->links = NULL;
-}
-
 void	initialization(t_data *data)
 {
 	initialize_data(data);
-	initialize_farm(data->farm);
 }
