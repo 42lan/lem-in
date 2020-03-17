1caca91a6395378a891dbc51b9bdc6e2b086db32
4slan <amalsago@student.42.fr>
2020-03-17 11:45:15 +0100

    improve: une ft_list_init() to initialize raw_input

--- sources/parsing/save_raw_input.c
+++ sources/parsing/save_raw_input.c
@@ -6,11 +6,15 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/16 21:37:29 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/16 21:39:13 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/17 11:40:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
+
+/*
+** This function saves received input from STDIN into a t_list
+*/
 
 t_list				*save_raw_input(void)
 {
@@ -18,19 +22,22 @@ t_list				*save_raw_input(void)
 	t_list_link		*raw_line;
 	t_list			*raw_input;
 
-	raw_input = NULL;
+	if (!(raw_input = ft_list_init()))
+	{
+		if (EMF)
+			ft_dprintf(2, "ft_list_init() failed in save_raw_input()\n");
+		return (NULL);
+	}
 	while (get_next_line(0, &line) > 0)
 	{
 		if (!(raw_line = ft_list_link_new(line, ft_strlen(line) + 1)))
 		{
+			// Need to free raw_input and line?
 			if (EMF)
 				ft_dprintf(2, "ft_list_link_new() failed in save_raw_input()");
 			return (NULL);
 		}
-		if (raw_input == NULL)
-			raw_input = ft_list_new(raw_line);
-		else
-			ft_list_push(raw_input, raw_line);
+		ft_list_push(raw_input, raw_line);
 		ft_strdel(&line);
 	}
 	return (raw_input);
