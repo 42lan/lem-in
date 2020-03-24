d9d0ca511795f16d694b8fd0e978f15542c7ff02
Adam BAISAGOUROV <adam_bai@protonmail.com>
2020-03-24 22:01:35 +0100

    commit for multiple rooms error

--- sources/parsing/hmap_add.c
+++ sources/parsing/hmap_add.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 11:44:35 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/24 12:59:40 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/24 21:49:27 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,27 @@ static void		hmap_collision_handler(t_hmap *hmap, t_room *room)
 	tmp->next = new;
 }
 
-void			hmap_add(t_hmap *hmap, t_room room)
+int			hmap_index(const char *key)
+{
+	int		hash;
+	size_t	i;
+	char	*byte;
+
+	byte = (char*)&hash;
+	i = -1;
+	while (key[++i] != '\0')
+		byte[i % 4] ^= key[i];
+	return (hash % HMAP_SIZE);
+}
+
+void			hmap_add(t_hmap *hmap, t_room *room)
 {
 	int			index;
 	t_room		*room_alloc;
 
-	room_alloc = (t_room *)ft_memalloc(sizeof(t_room));
-	ft_memcpy(room_alloc, &room, sizeof(t_room));
+	room_alloc = (t_room *)malloc(sizeof(*room));
+	ft_memcpy(room_alloc, room, sizeof(t_room));
+	ft_printf("room: %s || %s\n", room->name, room_alloc->name);
 	index = hmap_index(room_alloc->name);
 	if (hmap[index].room == NULL)
 		hmap[index].room = room_alloc;
