7986a30fbce01c0454f1d431530183c880f62910
4slan <amalsago@student.42.fr>
2020-03-25 23:19:55 +0100

    fix: remove extra new line

--- sources/debug/print.c
+++ sources/debug/print.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   debug.c                                            :+:      :+:    :+:   */
+/*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/25 16:10:47 by abaisago          #+#    #+#             */
-/*   Updated: 2020/03/25 19:45:46 by abaisago         ###   ########.fr       */
+/*   Updated: 2020/03/25 23:17:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		dbg_print_room(void *content, size_t size, unsigned pos, unsigned max)
 	int		ret;
 
 	room = content;
-	ret = ft_printf("[%s %d %d]\n", room->name, room->coord.x, room->coord.y);
+	ret = ft_printf("[%s %d %d]", room->name, room->coord.x, room->coord.y);
 	if (pos == max)
 		ret += ft_printf("\n");
 	return (ret);
