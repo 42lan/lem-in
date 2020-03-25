e407870e901476bc33eeb3017db3086da9e39998
Adam BAISAGOUROV <adam_bai@protonmail.com>
2020-03-25 20:18:33 +0100

    fix(debug): add debug.h

---
+++ includes/debug.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/25 19:50:23 by abaisago          #+#    #+#             */
+/*   Updated: 2020/03/25 19:51:26 by abaisago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEBUG_H
+# define DEBUG_H
+# include <stddef.h>
+
+int		dbg_print_room(void *content, size_t size, unsigned pos, unsigned max);
+
+/*
+** DEBUG_H
+*/
+#endif
