597a2815ac281b4851f6e1d615067a2fe15b7611
4slan <amalsago@student.42.fr>
2020-03-25 23:25:07 +0100

    feat: sceleton of the function

--- sources/parsing/get_links.c
+++ sources/parsing/get_links.c
@@ -6,40 +6,44 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 20:54:45 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/25 17:55:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/25 23:16:49 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lib.h"
 #include "parsing.h"
+#include "lib.h"
+#include "debug.h"
 
 #include <errno.h>
 #include <stddef.h>
 #include <string.h>
 
+int			room_namecmp(t_room *r1, char *r2)
+{
+    return (ft_strcmp(r1->name, r2));
+}
+
 void		inc_links(t_list *hmap, char *line)
 {
-	(void)hmap; (void)line;
-/*
 	int		i1;
 	int		i2;
 	char	*r1;
 	char	*r2;
 	t_list	*links;
+	t_room	*room;
 
 	if (!(links = ft_list_init()))
 		ft_printerr("lem-in: inc_links(malloc): %s\n", strerror(errno));
 	if (ft_strchr(line, '-'))
 	{
 		r1 = ft_strtok(line, "-");
-		ft_printf("r1: %s\n", r1);
 		r2 = ft_strtok(NULL, "-");
 		i1 = hmap_index(r1);
-		ft_printf("i1: %d\n", i1);
 		i2 = hmap_index(r2);
-		ft_printf(": %s\n", hmap[i1].room->name);
+		ft_list_print(&hmap[i1], dbg_print_room);
+		room = ft_list_find(&hmap[i1], r1, room_namecmp)->content;
+		ft_printf("%s\n", room->name);
 	}
-*/
 }
 
 void		get_links(t_list *hmap, char *line)
