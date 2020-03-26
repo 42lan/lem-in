3bcc5083929c80846e6da88139126ecbc13cc9ba
4slan <amalsago@student.42.fr>
2020-03-26 17:11:53 +0100

    feat: print size of connected links

--- sources/debug/print.c
+++ sources/debug/print.c
@@ -6,7 +6,7 @@
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/25 16:10:47 by abaisago          #+#    #+#             */
-/*   Updated: 2020/03/25 23:17:12 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/26 11:35:52 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ int		dbg_print_room(void *content, size_t size, unsigned pos, unsigned max)
 	int		ret;
 
 	room = content;
-	ret = ft_printf("[%s %d %d]", room->name, room->coord.x, room->coord.y);
+	ret = ft_printf("[%s %d,%d]", room->name, room->coord.x, room->coord.y);
+	if (room->links != NULL)
+		ret += ft_printf("(%d) ", room->links->size);
 	if (pos == max)
 		ret += ft_printf("\n");
 	return (ret);
