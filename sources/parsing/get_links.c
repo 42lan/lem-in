4f2ed80ae8b22a9fab53ccd9bc9f62476dde826c
4slan <amalsago@student.42.fr>
2020-03-26 17:14:12 +0100

    feat: definition of comlete get_links()

--- sources/parsing/get_links.c
+++ sources/parsing/get_links.c
@@ -6,43 +6,74 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 20:54:45 by amalsago          #+#    #+#             */
-/*   Updated: 2020/03/25 23:16:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/03/26 14:14:40 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "lib.h"
 #include "debug.h"
+#include "tools.h"
 
 #include <errno.h>
 #include <stddef.h>
 #include <string.h>
 
-int			room_namecmp(t_room *r1, char *r2)
+
+static void		get_room_names(char *line, char *room_name[2])
 {
-    return (ft_strcmp(r1->name, r2));
+	room_name[0] = ft_strtok(line, "-");
+	room_name[1] = ft_strtok(NULL, "-");
 }
 
-void		inc_links(t_list *hmap, char *line)
+static void		get_room_index(char *room_name[2], int *room_index)
 {
-	int		i1;
-	int		i2;
-	char	*r1;
-	char	*r2;
-	t_list	*links;
-	t_room	*room;
+	room_index[0] = hmap_index(room_name[0]);
+	room_index[1] = hmap_index(room_name[1]);
+}
 
-	if (!(links = ft_list_init()))
-		ft_printerr("lem-in: inc_links(malloc): %s\n", strerror(errno));
+static int		get_room_link(t_list *hmap, t_list_link *room_link[2],
+				int *room_index, char *room_name[2])
+{
+	room_link[0] = ft_list_find(&hmap[room_index[0]], room_name[0], room_namecmp);
+	room_link[1] = ft_list_find(&hmap[room_index[1]], room_name[1], room_namecmp);
+	if (!room_link[0] || !room_link[1])
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+static void		set_room_links(t_list_link *room_link[2])
+{
+	t_room		*room1;
+	t_room		*room2;
+
+	room1 = room_link[0]->content;
+	room2 = room_link[1]->content;
+	if (room1->links == NULL)
+		room1->links = ft_list_new(ft_list_link_new(room2, sizeof(t_room)));
+	else
+		ft_list_push(room1->links, ft_list_link_new(room2, sizeof(t_room)));
+	if (room2->links == NULL)
+		room2->links = ft_list_new(ft_list_link_new(room1, sizeof(t_room)));
+	else
+		ft_list_push(room2->links, ft_list_link_new(room1, sizeof(t_room)));
+}
+
+static void		add_links(t_list *hmap, char *line)
+{
+	char		*room_name[2];
+	int			room_index[2];
+	t_list_link	*room_link[2];
+
 	if (ft_strchr(line, '-'))
 	{
-		r1 = ft_strtok(line, "-");
-		r2 = ft_strtok(NULL, "-");
-		i1 = hmap_index(r1);
-		i2 = hmap_index(r2);
-		ft_list_print(&hmap[i1], dbg_print_room);
-		room = ft_list_find(&hmap[i1], r1, room_namecmp)->content;
-		ft_printf("%s\n", room->name);
+		get_room_names(line, room_name);
+		if (ft_strequ(room_name[0], room_name[1]))
+			return ;
+		get_room_index(room_name, room_index);
+		if (get_room_link(hmap, room_link, room_index, room_name) == FAILURE)
+			return ;
+		set_room_links(room_link);
 	}
 }
 
@@ -50,18 +81,16 @@ void		get_links(t_list *hmap, char *line)
 {
 	int		ret;
 
-	if (line[0] == '#')
-		ft_strdel(&line);
-	else
-	{
-		inc_links(hmap, line);
-		ft_strdel(&line);
-	}
+	if (!line)
+		return ;
+	if (line[0] != '#')
+		add_links(hmap, line);
+	ft_strdel(&line);
 	while ((ret = get_next_line(0, &line)) > 0)
 	{
 		if (line[0] == '#')
 			continue ;
-		inc_links(hmap, line);
+		add_links(hmap, line);
 		ft_strdel(&line);
 	}
 }
