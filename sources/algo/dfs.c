2291e97cc6d47f4f2c0efbb08c5188f50c48fef3
Adam BAISAGOUROV <adam_bai@protonmail.com>
2020-04-13 21:34:04 +0200

    feat: working version of dfs w/ cost

--- sources/algo/dfs.c
+++ sources/algo/dfs.c
@@ -6,7 +6,7 @@
 /*   By: abosch <abosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 16:11:29 by abosch            #+#    #+#             */
-/*   Updated: 2020/04/13 16:52:26 by abaisago         ###   ########.fr       */
+/*   Updated: 2020/04/13 21:24:07 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,43 +26,56 @@ int		dfs(t_room *start, t_room *target)
 	room = start;
 	room->pre[CUR] = room->index;
 	room->cost[CUR] = (cost = 0);
-	while (room != target)
+	while (1)
 	{
-		ft_printf("\n======| %s |=========< %u:%s\n",
+		ft_printf("\n===| %s || %u |=====< %s\n",
 			room->name, room->cost[CUR], ROOMS[room->pre[CUR]].name);
-		prev = room->index;
-		i = 0;
-		while (ROOMS[LINK_ARR[i]].pre[CUR] != UINT_MAX && i < LINK_SIZE)
-			++i;
-		if (i >= LINK_SIZE)
-			break;
-		ft_printf("while: %u/%u -> %s\n", i + 1, LINK_SIZE, ROOMS[LINK_ARR[i]].name);
-		if (i == LINK_SIZE || ROOMS[LINK_ARR[i]].cost[CUR] <= cost + 1)
+		if (room == target)
 		{
 			--cost;
-			room->pre[CUR] = room->pre[OLD];
-			room->cost[CUR] = room->cost[OLD];
 			room = ROOMS + room->pre[CUR];
-			ft_printf("we go back\n");
+			ft_printf("we go back at end\n=======================\n");
+			continue ;
+		}
+		prev = room->index;
+		i = 0;
+		while ((cost + 1 > ROOMS[LINK_ARR[i]].cost[CUR]
+			|| ROOMS[LINK_ARR[i]].pre[CUR] == room->index) && i < LINK_SIZE)
+			++i;
+		ft_printf("while: %u/%u -> %s:%u\n", i + 1, LINK_SIZE,
+			ROOMS[LINK_ARR[i]].name, ROOMS[LINK_ARR[i]].cost[CUR]);
+		if (i == LINK_SIZE)
+		{
+			if (room == start)
+				break;
+			else
+			{
+				--cost;
+				room = ROOMS + room->pre[CUR];
+				ft_printf("we go back\n");
+			}
 		}
 		else
 		{
 			++cost;
 			room = ROOMS + LINK_ARR[i];
+			room->pre[OLD] = room->pre[CUR];
+			room->cost[OLD] = room->cost[CUR];
 			room->pre[CUR] = prev;
 			room->cost[CUR] = cost;
 			ft_printf("We go deeper\n");
 		}
 		ft_printf("=================\n");
 	}
-	if (room != target)
+	if (target->pre[CUR] == UINT_MAX)
 	{
 		ft_printf("\nDFS: failure, target :|%s|: not found\n", target->name);
 		return (FAILURE);
 	}
 	else
 	{
-		ft_printf("\nDFS: success, target :|%s|: found\n", room->name);
+		ft_printf("\nDFS: success, target :|%s|: found with cost= %u\n",
+			target->name, target->cost[CUR]);
 		return (SUCCESS);
 	}
 }
