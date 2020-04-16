0f3458188c39365e8527a3877ffc140176ab5578
4slan <amalsago@student.42.fr>
2020-04-16 16:34:09 +0200

    feat: add room_indexcmp

--- includes/tools.h
+++ includes/tools.h
@@ -6,7 +6,7 @@
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/21 17:16:30 by abaisago          #+#    #+#             */
-/*   Updated: 2020/04/15 19:52:24 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/04/16 15:54:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 void	print_help(void);
 void	room_index(t_room *room, size_t size, unsigned pos, unsigned max);
 int		room_namecmp(t_room *room1, char *room2);
+int		room_indexcmp(unsigned room1, unsigned room2);
 void	reset_info(void);
 
 /*
