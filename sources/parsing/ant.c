31dba58b47b16217c071217f01451b9e6f3dab64
4slan <amalsago@student.42.fr>
2020-04-19 23:15:14 +0200

    fix: save pointer of line to free it at the end

--- sources/parsing/ant.c
+++ sources/parsing/ant.c
@@ -6,21 +6,23 @@
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/28 17:22:27 by abaisago          #+#    #+#             */
-/*   Updated: 2020/04/11 23:42:46 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/04/19 21:14:26 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "libft.h"
+#include "lib.h"
 
 #include "predicates.h"
 
 #include <errno.h>
 #include <string.h>
 
-unsigned	get_ants()
+unsigned	get_ants(void)
 {
 	char		*line;
+	char		*p_line;
 	long long	ants;
 	int			ret;
 
@@ -28,13 +30,13 @@ unsigned	get_ants()
 		ft_printerr("lem-in: get_ants(read): %s\n", strerror(errno));
 	if (ret == 0)
 		ft_printerr(ERROR);
+	p_line = line;
 	ants = ft_atoll(line);
 	if (overflowed(line, ants))
 		ft_printerr(E_ANTS);
-	line += ft_strskip(line, ft_isdigit);
-	line += ft_strskip_set(line, " \t");
+	line += ft_strspn(line, "0123456789 \t");
 	if (*line != '\0')
 		ft_printerr(E_NONDIG);
+	ft_strdel(&p_line);
 	return (ants);
 }
-
