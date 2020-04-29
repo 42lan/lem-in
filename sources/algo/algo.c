b3459765f4a0a436d0612a4de7b917335a046df9
aboscho <argan.bosch@free.fr>
2020-04-29 16:52:53 +0200

    fix: now lem-in handle maps with no connection between start and end

--- sources/algo/algo.c
+++ sources/algo/algo.c
@@ -6,7 +6,7 @@
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/16 22:01:45 by abaisago          #+#    #+#             */
-/*   Updated: 2020/04/28 17:31:49 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/04/29 16:49:36 by abosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 
 void	resolve()
 {
+	if (dfs(g_farm.start, g_farm.end, FULL) == FAILURE)
+		ft_printerr("Invalid map: No path between Start and End\n");
+	orient_path_to(g_farm.end);
+	/* print_paths_from(g_farm.start); */
+	reset_info();
 	while (dfs(g_farm.start, g_farm.end, FULL) == SUCCESS)
 	{
 		orient_path_to(g_farm.end);
