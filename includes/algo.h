9cffc160e0584b70e01687035cd2272b457f91b6
4slan <amalsago@student.42.fr>
2020-05-02 16:47:07 +0200

    improve: compute cost with total travarsable rooms on map

--- includes/algo.h
+++ includes/algo.h
@@ -6,7 +6,7 @@
 /*   By: abosch <abosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 18:25:39 by abosch            #+#    #+#             */
-/*   Updated: 2020/04/16 22:03:27 by abaisago         ###   ########.fr       */
+/*   Updated: 2020/04/29 17:42:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void		remove_deadend(void);
 int			dfs(t_room *start, t_room *target, t_byte type);
 void		orient_path_to(t_room *target);
 void		resolve();
+unsigned	cost_by_path(t_room *target);
 
 #endif
