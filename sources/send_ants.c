805914c999f56799e369284cbbb8edec3a6ff7b7
4slan <amalsago@student.42.fr>
2020-05-19 20:13:32 +0200

    feat: send ants according dispatch

--- sources/send_ants.c
+++ sources/send_ants.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 17:33:06 by amalsago          #+#    #+#             */
-/*   Updated: 2020/05/19 20:08:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/05/19 20:12:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 #include "colors.h"
 
 #include <unistd.h>
-static void		move_ant(t_room *prev, t_room *curr)
+static void			move_ant(t_room *prev, t_room *curr, unsigned k)
 {
 	static unsigned	ant_id = 0;
 
 	if (curr == START && g_farm.ants_start > 0)
 	{
 		if (g_farm.ants_start > 0)
+		{
+			if (g_farm.ants_by_path[k] > 0)
+				g_farm.ants_by_path[k] -= 1;
 			ant_id += 1;
+		}
 		else
 			ant_id = 0;
 		g_farm.ants_start -= 1;
@@ -34,27 +38,28 @@ static void		move_ant(t_room *prev, t_room *curr)
 		if (prev == END)
 		{
 			g_farm.ants_end += 1;
-//			ft_printf(SGR_FG_RED"L%d-%s "SGR_NORMAL, prev->ant_id, prev->name);
-			ft_printf("L%d-%s ", prev->ant_id, prev->name);
+			/* ft_printf(SGR_FG_RED"L%d-%s "SGR_NORMAL, prev->ant_id, prev->name); */
+			/* ft_printf("L%d-%s ", prev->ant_id, prev->name); */
+			ft_printf(SGR_BOLD"L%d-%s "SGR_NORMAL, prev->ant_id, prev->name);
 		}
 		else
 			ft_printf("L%d-%s ", prev->ant_id, prev->name);
 	}
 }
 
-static void		send_ants_helper(t_room *prev, t_room *curr)
+static void			send_ants_helper(t_room *prev, t_room *curr, unsigned k)
 {
-	unsigned	j;
+	unsigned		j;
 
 	j = -1;
-	move_ant(prev, curr);
+	move_ant(prev, curr, k);
 	while (++j < curr->LINK_LEN)
 	{
 		if (curr->link.dir[j] == ALLOWED)
 		{
 			prev = curr;
 			curr = ROOMS + curr->link.arr[j];
-			move_ant(prev, curr);
+			move_ant(prev, curr, k);
 			j = -1;
 		}
 	}
@@ -65,6 +70,14 @@ static unsigned		start_end_connected(void)
 	unsigned		i;
 
 	i = -1;
+	while (++i < START->LINK_LEN)
+		if (ROOMS + START->link.arr[i] == END)
+		{
+			ft_printf("Connected\n");
+			return (SUCCESS);
+		}
+	return (FAILURE);
+}
 
 static void			one_move(void)
 {
@@ -74,17 +87,28 @@ static void			one_move(void)
 	ft_printf("\n");
 }
 
+void				send_ants(void)
+{
+	unsigned		i;
+	unsigned		k;
+	unsigned		moves;
+
 	moves = 0;
 	if (start_end_connected() == SUCCESS)
 		one_move();
 	while (g_farm.ants_end != g_farm.ants_total)
 	{
-		while (g_farm.ants_end != g_farm.ants_total && ++i < END->LINK_LEN)
-			if (END->link.dir[i] == ALLOWED)
-				send_ants_helper(END, ROOMS + END->link.arr[i]);
-		ft_putchar('\n');
-		moves += 1;
 		i = -1;
+		k = -1;
+		moves += 1;
+		ft_printf(SGR_BOLD SGR_FG_YELLOW"%-5d"SGR_NORMAL, moves);
+		while (g_farm.ants_end != g_farm.ants_total && ++i < END->LINK_LEN)
+			if (END->link.dir[i] == ALLOWED)// && g_farm.ants_by_path[++k] > 0)
+				send_ants_helper(END, ROOMS + END->link.arr[i], ++k);
+		ft_putchar('\n');
 	}
+	/* i = -1; */
+	/* while (++i < g_farm.nb_paths) */
+	/* 		ft_printf("%i path have %d ants\n", i, g_farm.ants_by_path[i]); */
 //	ft_printf(SGR_INVERSE" Total moves: %d \n"SGR_NORMAL, moves);
 }
