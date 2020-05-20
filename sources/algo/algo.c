bc7a6e1a4832f3ec48fa9b8436c9974086463a52
4slan <amalsago@student.42.fr>
2020-05-20 20:36:43 +0200

    feat: recount cost and update global variables

--- sources/algo/algo.c
+++ sources/algo/algo.c
@@ -6,7 +6,7 @@
 /*   By: abaisago <adam_bai@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/16 22:01:45 by abaisago          #+#    #+#             */
-/*   Updated: 2020/05/08 00:55:18 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/05/20 20:35:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,14 @@ void	resolve()
 		if (curr_cost < last_cost)
 		{
 			/* ft_printf(SGR_FG_GREEN"CURR COST : %u\n"SGR_NORMAL, curr_cost); */
-			ft_printf("==========\n");
-			print_paths_from(g_farm.start);
-			ft_printf("==========\n\n");
+			/* print_paths_from(g_farm.start); */
 			last_cost = curr_cost;
 		}
 		else
 		{
 			/* ft_printf(SGR_FG_RED"LAST ORIENT_PATH_TO WILL BE REVERTED\n"SGR_NORMAL); */
 			orient_path_to(END, REV_YES);
+			get_cost();
 			break ;
 		}
 		reset_info();
