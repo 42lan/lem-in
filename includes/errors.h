6568c29255a92ad0ea5dc9c6eb13c499e15081c6
4slan <amalsago@student.42.fr>
2020-05-23 12:20:55 +0200

    feat: correct minimum acceptable number of ants from 0 to 1

--- includes/errors.h
+++ includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 17:02:15 by amalsago          #+#    #+#             */
-/*   Updated: 2020/05/08 00:54:34 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/05/23 12:18:15 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define ERRORS_H
 
 # define ERROR		"ERROR\n"
-# define E_ANTS		"ERROR: 1st line must be a number between 0 and UINT_MAX\n"
+# define E_ANTS		"ERROR: 1st line must be a number between 1 and UINT_MAX\n"
+# define E_NOANT	"ERROR: no ants are provided in the current map\n"
 # define E_NONDIG	"ERROR: 1st line contains non-digit character(s)\n"
 # define E_SE_DEF	"ERROR: START and/or END rooms aren't defined\n"
 # define E_SE_LINK	"ERROR: START and/or END rooms haven't at least one link\n"
