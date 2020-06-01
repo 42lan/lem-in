6bfeafe329ecb89c7a105b90c4cdd3e87722b1e7
4slan <amalsago@student.42.fr>
2020-06-01 14:43:18 +0200

    feat: add variable to show state of exploration

--- sources/algo/dfs.c
+++ sources/algo/dfs.c
@@ -6,7 +6,7 @@
 /*   By: abosch <abosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 16:11:29 by abosch            #+#    #+#             */
-/*   Updated: 2020/05/30 18:31:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/01 13:55:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 */
 void	update_info(t_room **curr, t_room *prev, unsigned offset)
 {
-	/* (DEBUGP) ? ft_printf("We go deeper\n") : 0; */
+	(DEBUGP) ? ft_printf("We go deeper\n") : 0;
 	t_room	*room;
 
 	room = *curr;
@@ -39,7 +39,7 @@ void	update_info(t_room **curr, t_room *prev, unsigned offset)
 */
 void	backtrace_passive(t_room **room)
 {
-	/* (DEBUGP) ? ft_printf("{byellow}{fred}we go back{}\n") : 0; */
+	(DEBUGP) ? ft_printf("{byellow}{fred}we go back{}\n") : 0;
 	*room = ROOMS + (*room)->pre[CUR];
 }
 
@@ -48,6 +48,7 @@ void	backtrace_passive(t_room **room)
 */
 void	backtrace_destructive(t_room **curr, t_room *prev)
 {
+	ft_printf("{fblack}{bwhite}-------BACK TRACK DESTRUCTIVE AT %s ------{}\n", (*curr)->name);
 	/* * garder dans la memoire d'ou on est venu en faisant backtrace_destructiv() */
 	/* * verifier que dans le nodes connectes il n'y a pas de pre[CUR]+cost[CUR+1](nom de node en question + 1) */
 	t_room		*room;
@@ -77,8 +78,9 @@ void	backtrace_destructive(t_room **curr, t_room *prev)
 	}
 	else
 	{
+		if (*curr != START)
+			backtrace_destructive(curr, prev);
 		*curr = ROOMS + room->pre[CUR];
-		backtrace_destructive(curr, prev);
 		return ;
 	}
 }
@@ -90,16 +92,16 @@ static unsigned	choose_link(t_room *room)
 
 	i = 0;
 	j = 0;
-	/* (DEBUGP) ? show_orien(room) : 0; */
+	(DEBUGP) ? show_orien(room) : 0;
 	while (i < LINK_SIZE && LINK_DIR[i] != ALLOWED)
 		++i;
-	/* (DEBUGP) ? ft_printf("i = %d\n", i) : 0; */
+	(DEBUGP) ? ft_printf("i = %d\n", i) : 0;
 	while (j < LINK_SIZE && LINK_ARR[j] != room->pre[CUR])
 		++j;
-	/* (DEBUGP) ? ft_printf("j = %d\n", j) : 0; */
+	(DEBUGP) ? ft_printf("j = %d\n", j) : 0;
 	if (i == LINK_SIZE || LINK_DIR[j] != DUPLEX)
 	{
-		/* (DEBUGP) ? ft_printf("cas standard\n") : 0; */
+		(DEBUGP) ? ft_printf("cas standard\n") : 0;
 		i = 0;
 		while (i < LINK_SIZE && (ROOMS[LINK_ARR[i]].flags & F_DEAD
 			|| room->cost[CUR] + 1 >= ROOMS[LINK_ARR[i]].cost[CUR]
@@ -110,26 +112,40 @@ static unsigned	choose_link(t_room *room)
 	}
 	else if (ROOMS[LINK_ARR[i]].cost[CUR] <= room->cost[CUR] + 1)
 		return (LINK_SIZE);
-	/* else if (DEBUGP) ft_printf("On arrive sur un path on doit le remonter\n"); */
+	else if (DEBUGP) ft_printf("On arrive sur un path on doit le remonter\n");
 	return (i);
 }
 
 static t_room	*traverse(t_room *start, t_room *room, unsigned offset)
 {
 	t_room		*prev;
+	static int	exploration = 0;
 
 	prev = room;
 	if (offset == LINK_SIZE)
 	{
 		if (room == start)
+		{
+			exploration = 1;
 			return (NULL);
-		/* else if (room != END) */
-		/* 	backtrace_destructive(&room, prev); */
+		}
+		else if (exploration == 1)
+		{
+			backtrace_destructive(&room, prev);
+			exploration = 0;
+			/* exploration = 1; */
+		}
 		else // If END is reached, go back passivly
+		{
 			backtrace_passive(&room);
+			exploration = 0;
+		}
 	}
 	else
+	{
 		update_info(&room, prev, offset);
+		exploration = 1;
+	}
 	return (room);
 }
 
@@ -137,18 +153,19 @@ static int		finish(t_room *target)
 {
 	if (target->pre[CUR] == UINT_MAX)
 	{
-		/* (DEBUGP) ? ft_printf("\nDFS: failure, target :|%s|: not found\n", target->name) : 0; */
+		(DEBUGP) ? ft_printf("\nDFS: failure, target :|%s|: not found\n", target->name) : 0;
 		return (FAILURE);
 	}
 	else
 	{
-		/* (DEBUGP) ? ft_printf("\nDFS: success, target :|%s|: (%u steps)\n", target->name, target->cost[CUR]) : 0; */
+		(DEBUGP) ? ft_printf("\nDFS: success, target :|%s|: (%u steps)\n", target->name, target->cost[CUR]) : 0;
 		return (SUCCESS);
 	}
 }
 
 int		dfs(t_room *start, t_room *target, t_byte type)
 {
+	ft_printf("\n\n\n");
 	t_room		*room;
 	unsigned	offset;
 
@@ -157,25 +174,25 @@ int		dfs(t_room *start, t_room *target, t_byte type)
 	room->cost[CUR] = 0;
 	while (1)
 	{
-		/* (DEBUGP) ? ft_printf("\n===| %s || %u |=====< %s\n", room->name, room->cost[CUR], ROOMS[room->pre[CUR]].name) : 0; */
+		(DEBUGP) ? ft_printf("\n===| %s || %u |=====< %s\n", room->name, room->cost[CUR], ROOMS[room->pre[CUR]].name) : 0;
 		if (room == target)
 		{
 			room = ROOMS + room->pre[CUR];
-			/* (DEBUGP) ? ft_printf("we go back at end\n=======================\n") : 0; */
+			(DEBUGP) ? ft_printf("we go back at end\n=======================\n") : 0;
 			if (type == FULL)
 				continue ;
 			else
 				break ;
 		}
 		offset = choose_link(room);
-		/* (DEBUGP) ? ft_printf("offset = %u\n", offset) : 0; */
+		(DEBUGP) ? ft_printf("offset = %u\n", offset) : 0;
 		if ((room = traverse(start, room, offset)) == NULL)
 		{
 			start->cost[CUR] = 0;
 			break ;
 		}
-		/* (DEBUGP) ? ft_printf("=================\n") : 0; */
+		(DEBUGP) ? ft_printf("=================\n") : 0;
 	}
-	/* (DEBUGP) ? ft_printf("before finish\n") : 0; */
+	(DEBUGP) ? ft_printf("before finish\n") : 0;
 	return (finish(target));
 }
