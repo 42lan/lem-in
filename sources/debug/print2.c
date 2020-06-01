dae168f388d45497fa4e8c5c16f9a63f182c36c1
Adam BAISAGOUROV <adam_bai@protonmail.com>
2020-06-02 00:00:19 +0200

    feat(debug): improve print_map_cost debug output

--- sources/debug/print2.c
+++ sources/debug/print2.c
@@ -59,7 +59,7 @@ void	print_map_cost(void)
 	i = 0;
 	while (i < g_farm.size)
 	{
-		ft_printf("%s cur : %u - old : %u\n", ROOMS[i].name, ROOMS[i].cost[CUR],  ROOMS[i].cost[OLD]);
+		ft_printf("%-7s pre: %3u/%-10u | cost: %3u/%-10u\n", ROOMS[i].name, ROOMS[i].pre[CUR],  ROOMS[i].pre[OLD], ROOMS[i].cost[CUR], ROOMS[i].cost[OLD] );
 		i++;
 	}
 }
